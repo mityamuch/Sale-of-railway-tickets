08a570b7c642a2a61f01f3544ad9e7c177a802da
Oleg Smirnov <87186713+olejenkasmirnov@users.noreply.github.com>
2023-11-15 23:46:04 +0300

    my commit (#3)

--- services/StationService.py
+++ services/StationService.py
@@ -7,6 +7,8 @@ class StationService:
     async def add_station(station_data):
         await db.stations.insert_one(station_data)
 
+
+
     @staticmethod
     async def get_station(station_id):
         return await db.stations.find_one({"_id": station_id})
